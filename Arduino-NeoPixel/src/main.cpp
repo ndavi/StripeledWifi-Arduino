6ca82d980835b4c17a595a6d547d2c089db50af2
ndavid <nicolas.david@hardis.fr>
2018-12-31 05:43:50 +0100

    Switched to FastLed, added MQTT protocol to ESP

--- Arduino-NeoPixel/src/main.cpp
+++ Arduino-NeoPixel/src/main.cpp
@@ -1,13 +1,13 @@
 
-#include <Adafruit_NeoPixel.h>
+#include <FastLED.h>
 #include <Arduino.h>
 #include <ArduinoJson.h>
    
-// Neopixel settings
 const int numLeds = 300; // change for your setup
 const int numberOfChannels = numLeds * 3; // Total number of channels you want to receive (1 led = 3 channels)
 const byte dataPin = 2;
-Adafruit_NeoPixel leds = Adafruit_NeoPixel(numLeds, dataPin, NEO_GRB + NEO_KHZ400);
+CRGB leds[numLeds];
+
 
 const int startUniverse = 1; // CHANGE FOR YOUR SETUP most software this is 1, some software send out artnet first universe as 0.
 
@@ -20,36 +20,41 @@ int previousDataLength = 0;
 
 void initTest()
 {
-  for (int i = 0 ; i < numLeds ; i++)
-    leds.setPixelColor(i, 127, 0, 0);
-  leds.show();
+  for (int i = 0 ; i < numLeds ; i++) {
+    leds[i] = CRGB(127, 0, 0);
+  }
+  FastLED.show();
   delay(500);
-  for (int i = 0 ; i < numLeds ; i++)
-    leds.setPixelColor(i, 0, 127, 0);
-  leds.show();
+  for (int i = 0 ; i < numLeds ; i++) {
+    leds[i] = CRGB(0, 127, 0);
+  }
+  FastLED.show();
   delay(500);
-  for (int i = 0 ; i < numLeds ; i++)
-    leds.setPixelColor(i, 0, 0, 127);
-  leds.show();
+  for (int i = 0 ; i < numLeds ; i++) {
+    leds[i] = CRGB(0, 0, 127);
+  }
+  FastLED.show();
   delay(500);
-  for (int i = 0 ; i < numLeds ; i++)
-    leds.setPixelColor(i, 0, 0, 0);
-  leds.show();
+  for (int i = 0 ; i < numLeds ; i++) {
+    leds[i] = CRGB(0, 0, 0);
+  }
+  FastLED.show();
 }
 
 void onDmxFrame(uint16_t universe, uint16_t length, uint8_t sequence, uint8_t* data)
 {
   sendFrame = 1;
-  // set brightness of the whole strip 
+  // set brightness of the whole strip
   if (universe == 15)
   {
-    leds.setBrightness(data[0]);
-    leds.show();
+    FastLED.setBrightness(data[0]);
+    FastLED.show();
   }
 
   // Store which universe has got in
-  if ((universe - startUniverse) < maxUniverses)
+  if ((universe - startUniverse) < maxUniverses) {
     universesReceived[universe - startUniverse] = 1;
+  }
 
   for (int i = 0 ; i < maxUniverses ; i++)
   {
@@ -66,13 +71,13 @@ void onDmxFrame(uint16_t universe, uint16_t length, uint8_t sequence, uint8_t* d
   {
     int led = i + (universe - startUniverse) * (previousDataLength / 3);
     if (led < numLeds)
-      leds.setPixelColor(led, data[i * 3], data[i * 3 + 1], data[i * 3 + 2]);
+      leds[led] = CRGB(data[i * 3], data[i * 3 + 1], data[i * 3 + 2]);
   }
-  previousDataLength = length;     
-  
+  previousDataLength = length;
+
   if (sendFrame)
   {
-    leds.show();
+    FastLED.show();
     // Reset universeReceived to 0
     memset(universesReceived, 0, maxUniverses);
   }
@@ -82,12 +87,12 @@ void setup()
 {
   Serial.begin(115200);
   Serial1.begin(115200);
-  leds.begin();
+  FastLED.addLeds<WS2812, dataPin, GRB>(leds, numLeds);
   initTest();
-
 }
 
 void loop()
 {
-  Serial.println(Serial1.read());
+  Serial.print("COUCOU");
+  //Serial.println(Serial1.read());
 }
--- ESP8266-ArtnetClient/src/main.cpp
+++ ESP8266-ArtnetClient/src/main.cpp
@@ -1,31 +1,64 @@
 
-/*
-This example will receive multiple universes via Artnet and control a strip of ws2811 leds via 
-Adafruit's NeoPixel library: https://github.com/adafruit/Adafruit_NeoPixel
-This example may be copied under the terms of the MIT license, see the LICENSE file for details
-*/
-
 #include <ESP8266WiFi.h>
 #include <WiFiUdp.h>
 #include <ArtnetWifi.h>
 #include <Arduino.h>
 #include <ArduinoJson.h>
+#include <ESP8266mDNS.h>
+#include <PubSubClient.h>
+#include <ArduinoOTA.h>
+
+
 
 
 //Wifi settings
 const char* ssid = "ssid";
 const char* password = "pAsSwOrD";
 
-// Neopixel settings
-const int numLeds = 300; // change for your setup
-const int numberOfChannels = numLeds * 3; // Total number of channels you want to receive (1 led = 3 channels)
+/**************************** FOR OTA **************************************************/
+#define SENSORNAME "nico-stripe" //change this to whatever you want to call your device
+#define OTApassword "pass" //the password you will need to enter to upload remotely via the ArduinoIDE
+int OTAport = 8266;
 
+#pragma region artnetInit
+/****************************** -- ARTNET INIT ***********************************/
+
+const int numLeds = 300; 
+const int numberOfChannels = numLeds * 3; 
 // Artnet settings
 ArtnetWifi artnet;
 const int startUniverse = 1; // CHANGE FOR YOUR SETUP most software this is 1, some software send out artnet first universe as 0.
 
 StaticJsonBuffer<200> jsonBuffer;
 JsonObject& artnetData = jsonBuffer.createObject();
+#pragma endregion
+
+#pragma region mqttInit
+const char* mqtt_server = "your.MQTT.server.ip";
+const char* mqtt_username = "yourMQTTusername";
+const char* mqtt_password = "yourMQTTpassword";
+const int mqtt_port = 1883;
+
+/************* MQTT TOPICS **************************/
+const char* light_state_topic = "stripled/state";
+const char* light_set_topic = "stripeled/set";
+
+const char* on_cmd = "ON";
+const char* off_cmd = "OFF";
+const char* effect = "solid";
+String effectString = "solid";
+String oldeffectString = "solid";
+
+
+
+/****************************************FOR JSON***************************************/
+const int BUFFER_SIZE = JSON_OBJECT_SIZE(10);
+#define MQTT_MAX_PACKET_SIZE 512
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+#pragma endregion mqttInit
 
 // connect to wifi – returns true if successful or false if not
 boolean ConnectWifi(void)
@@ -33,6 +66,7 @@ boolean ConnectWifi(void)
   boolean state = true;
   int i = 0;
 
+  WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
   Serial.println("");
   Serial.println("Connecting to WiFi");
@@ -63,6 +97,26 @@ boolean ConnectWifi(void)
   return state;
 }
 
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    if (client.connect(SENSORNAME, mqtt_username, mqtt_password)) {
+      Serial.println("connected");
+      client.subscribe(light_set_topic);
+      //setColor(0, 0, 0);
+      //sendState();
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
 
 void onDmxFrame(uint16_t universe, uint16_t length, uint8_t sequence, uint8_t* data)
 {
@@ -73,18 +127,97 @@ void onDmxFrame(uint16_t universe, uint16_t length, uint8_t sequence, uint8_t* d
   artnetData.printTo(Serial);
 }
 
+/********************************** START CALLBACK*****************************************/
+void callback(char* topic, byte* payload, unsigned int length) {
+  //Serial.print("Message arrived [");
+  //Serial.print(topic);
+  //Serial.print("] ");
+
+  Serial.print("payload + length");
+}
+
+void otaSetup() {
+  client.setServer(mqtt_server, mqtt_port);
+  client.setCallback(callback);
+
+  //OTA SETUP
+  ArduinoOTA.setPort(OTAport);
+  // Hostname defaults to esp8266-[ChipID]
+  ArduinoOTA.setHostname(SENSORNAME);
+
+  // No authentication by default
+  ArduinoOTA.setPassword((const char *)OTApassword);
+
+  ArduinoOTA.onStart([]() {
+    Serial.println("Starting");
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+    else if (error == OTA_END_ERROR) Serial.println("End Failed");
+  });
+  ArduinoOTA.begin();
+
+  Serial.println("Ready");
+  Serial.print("IP Address: ");
+  Serial.println(WiFi.localIP());
+}
+
+
+void sendState() {
+  StaticJsonBuffer<BUFFER_SIZE> jsonBuffer;
+
+  JsonObject& root = jsonBuffer.createObject();
+
+  // jsonState = Serial.read();
+
+
+  /*root["state"] = (stateOn) ? on_cmd : off_cmd;
+  JsonObject& color = root.createNestedObject("color");
+  color["r"] = red;
+  color["g"] = green;
+  color["b"] = blue;
+
+  root["brightness"] = brightness;
+  root["effect"] = effectString.c_str();
+
+
+  char buffer[root.measureLength() + 1];
+  root.printTo(buffer, sizeof(buffer));
+
+  client.publish(light_state_topic, buffer, true);*/
+}
+
 void setup()
 {
   Serial.begin(115200);
   ConnectWifi();
+  otaSetup();
   artnet.begin();
 
   // this will be called for each packet received
   artnet.setArtDmxCallback(onDmxFrame);
+  
 }
 
 void loop()
 {
   // we call the read function inside the loop
   artnet.read();
+  DynamicJsonBuffer jsonBuffer(1024);
+  JsonObject& root = jsonBuffer.parseObject(Serial);
+  if(root["true"]) {
+      sendState();
+  }
+  //const char* sensor = root["sensor"];
+  //Serial.println(sensor);
 }
